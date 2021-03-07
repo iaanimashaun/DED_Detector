68dbdfffd4fd0d095675eeb825ef416d40156c58
Ibrahim Animashaun <iaanimashaun@yahoo.com>
2021-03-07 00:21:35 +0000

    Update medhack.py

--- medhack.py
+++ medhack.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
+#import numpy as np
+#import pandas as pd
 import os 
-import matplotlib.pyplot as plt 
+#import matplotlib.pyplot as plt 
 #import tensorflow  
 from tensorflow import keras
 #from keras.models import load_model
