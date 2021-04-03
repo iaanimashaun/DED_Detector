21f67c38ecfa1c325fc6b560bd6601a1c19b9f04
Ibrahim Animashaun <iaanimashaun@yahoo.com>
2021-04-03 07:40:40 +0100

    Update app.py

--- ded-detector_demo/app.py
+++ ded-detector_demo/app.py
@@ -1,4 +1,4 @@
-### Script for CS329s ML Deployment Lec 
+
 import os
 import json
 import requests
@@ -13,12 +13,12 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 
 
-# Setup environment credentials (you'll need to change these)
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'ded-detector-4dcd64335a30.json' # "daniels-dl-playground-4edbcb2e6e37.json" # change for your GCP key
+# Setup environment credentials 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'ded-detector-4dcd64335a30.json' # change for your GCP key
 PROJECT = "ded-detector" # change for your GCP project
 REGION = "us-central1" # change for your GCP region (where your model is hosted)
 
-### Streamlit code (works as a straigtht-forward script) ###
+### Streamlit code ###
 
 st.title("Welcome to DED Detector")
 st.header("Detect Diabetic Eye Disease")
@@ -63,24 +63,13 @@ def make_prediction(image, model, class_names):
     return image, not_retina, 0
 
 # Pick the model version
-choose_model = 'Model'#st.sidebar.selectbox(
-    #"Pick model you'd like to use",
-   # (
-        #("Model") # original 10 classes
-     #"Model 2", # (11 food classes)", # original 10 classes + donuts
-     #"Model 3" )#(11 food classes + non-food class)") # 11 classes (same as above) + not_food class
-#)
+choose_model = 'Model'
 
 # Model choice logic
 if choose_model == "Model":
     CLASSES = classes_and_models["model_1"]["classes"]
     MODEL = classes_and_models["model_1"]["model_name"]
-# elif choose_model == "Model 2":
-#     CLASSES = classes_and_models["model_2"]["classes"]
-#     MODEL = classes_and_models["model_2"]["model_name"]
-# else:
-#     CLASSES = classes_and_models["model_3"]["classes"]
-#     MODEL = classes_and_models["model_3"]["model_name"]
+
 
 # Display info about model and classes
 if st.checkbox("Show stages of Diabetic Eye Disease"):
@@ -133,32 +122,6 @@ if session_state.pred_button:
                Confidence: {session_state.pred_conf:.3f}")
     else:
         st.write(f"Prediction: {session_state.pred_class}")
-
-    # Create feedback mechanism (building a data flywheel)
-    # session_state.feedback = st.selectbox(
-    #     "Is this correct?",
-    #     ("Select an option", "Yes", "No"))
-    # if session_state.feedback == "Select an option":
-    #     pass
-    # elif session_state.feedback == "Yes":
-    #     st.write("Thank you for your feedback!")
-    #     # Log prediction information to terminal (this could be stored in Big Query or something...)
-    #     print(update_logger(image=session_state.image,
-    #                         model_used=MODEL,
-    #                         pred_class=session_state.pred_class,
-    #                         pred_conf=session_state.pred_conf,
-    #                         correct=True))
-    # elif session_state.feedback == "No":
-    #     session_state.correct_class = st.text_input("What should the correct label be?")
-    #     if session_state.correct_class:
-    #         st.write("Thank you for that, we'll use your help to make our model better!")
-    #         # Log prediction information to terminal (this could be stored in Big Query or something...)
-    #         print(update_logger(image=session_state.image,
-    #                             model_used=MODEL,
-    #                             pred_class=session_state.pred_class,
-    #                             pred_conf=session_state.pred_conf,
-    #                             correct=False,
-    #                             user_label=session_state.correct_class))
 
 
             
